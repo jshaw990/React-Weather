65d181e3922719b4254003d2207e16604d9c8526
Jayden Shaw <jayden.shaw@gmail.com>
2020-05-13 09:10:54 -0600

    Filtering responses by time

--- src/WeekContainer.js
+++ src/WeekContainer.js
@@ -1,13 +1,33 @@
 import React from 'react';
+import weatherKey from './secret';
 
 class WeekContainer extends React.Component {
+    state = {
+        fullData: [],
+        dailyData: []
+    }
+
+    componentDidMount = () => {
+        const weatherURL = `http://api.openweathermap.org/data/2.5/forecast?zip=11102&units=imperial&APPID=${weatherKey.weatherKey}`
+        
+        fetch(weatherURL)
+        .then(res => res.json())
+        .then(data => {
+            const dailyData = data.list.filter(reading => reading.dt_txt.includes('18:00:00'))
+            this.setState({
+                fullData: data.list,
+                dailyData: dailyData
+            }, () => console.log(this.state))
+        })
+    }
     render() {
+
         return (
             <div>
-                <h1>Hello World!</h1>
+                <h1>Hello World</h1>
             </div>
         )
     }
 }
 
-export default WeekContainer;
+export default WeekContainer
