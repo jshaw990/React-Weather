a4aad86db2205f31fbe7d9c3338086965a184381
Jayden Shaw <jayden.shaw@gmail.com>
2020-05-13 09:22:54 -0600

    Implementing moment.js to daily forcast cards

---
+++ src/Card.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Card.css';
+
+const moment = require('moment');
+
+class Card extends React.Component {
+    render() {
+        let newDate = new Date();
+        const weekday = this.props.day.dt * 1000
+        newDate.setTime(weekday)
+
+        const imgURL = "owf owf-"+ this.props.day.weather[0].id +"owf-5x red"
+
+        return (
+            <div className="col-auto">
+                <div className="card bg-light">
+                    <h3 className="card-title">{moment(newDate).format('dddd')}</h3>
+                    <p className="text-muted">{moment(newDate).format('MMMM Do, h:mm a')}</p>
+                    <i classname={imgURL}></i>
+                    <h2>{Math.round(this.props.day.main.temp)} °F</h2>
+                    <div className="card-body">
+                        <p className="card-text">{this.props.day.weather[0].description}</p>
+                        <button className="btn btn-dark btn-outline-light">See Hourly Forcast</button>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Card;
